5183faf88299784672d0507631ce262ded8b3c39
Dag <dagmara.szadkowska@wp.pl>
2024-07-09 14:14:02 +0200

    styles: add test version of background animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade1: 'fade1 15s infinite',
+        fade2: 'fade2 15s infinite',
+        fade3: 'fade3 15s infinite',
+      },
+      keyframes: {
+        fade1: {
+          '0%, 26.66%': { opacity: '1', zIndex: '1' },
+          '33.33%, 100%': { opacity: '0', zIndex: '0' },
+        },
+        fade2: {
+          '0%, 26.66%': { opacity: '0', zIndex: '0' },
+          '33.33%, 60%': { opacity: '1', zIndex: '1' },
+          '66.66%, 100%': { opacity: '0', zIndex: '0' },
+        },
+        fade3: {
+          '0%, 60%': { opacity: '0', zIndex: '0' },
+          '66.66%, 93.33%': { opacity: '1', zIndex: '1' },
+          '100%': { opacity: '0', zIndex: '0' },
+        },
+      },
+    },
   },
   plugins: [],
 };
